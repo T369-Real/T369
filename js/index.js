166db81fa325236683f41b91f5f527c3767df99f
T369 <codex_150@protonmail.com>
2021-06-14 12:35:20 -0400

    :trollface:

--- js/index.js
+++ js/index.js
@@ -1,3 +1,3 @@
 function poco(){
     alert('muy pocas cosas se hacer con esta tecnologia💩');
-}
+};
---
+++ js/particles.js
@@ -0,0 +1,9 @@
+//awesome https://marcbruederlin.github.io/particles.js/
+
+window.onload = function() {
+    Particles.init({
+    selector: '.background',
+    speed: '2',
+    maxParticles: '100',
+    });
+};
